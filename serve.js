23d4ab480eb36611267398ec26dff4b272c26e09
axelo <axel.olsson@gmail.com>
2014-10-04 16:31:15 +0200

    yes

--- serve.js
+++ serve.js
@@ -1,8 +1,15 @@
-var connect = require('connect'),
-    directory = '.';
+var http = require('http');
 
-connect()
-    .use(connect.static(directory))
-    .listen(80);
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+});
+
+server.listen(80);
 
 console.log('Listening on port 80.');
